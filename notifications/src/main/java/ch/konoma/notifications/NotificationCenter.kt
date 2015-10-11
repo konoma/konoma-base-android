b13a6092a6605dac24a2e768d9e961e441bd8f66
Markus Gasser <markus.gasser@konoma.ch>
2015-10-11 16:59:29 +0200

    Register GcmReceiver on demand

--- notifications/src/main/java/ch/konoma/notifications/NotificationCenter.kt
+++ notifications/src/main/java/ch/konoma/notifications/NotificationCenter.kt
@@ -11,16 +11,37 @@ import com.microsoft.windowsazure.messaging.NotificationHub
 public class NotificationCenter(val context: Context, val senderIdentifier: String, val hubName: String, val connectionString: String) {
 
     private val notificationHub = NotificationHub(hubName, connectionString, context)
+    private val preferences = context.getSharedPreferences("ch.konoma.notifications", Context.MODE_PRIVATE)
 
 
     /// Registering
 
-    public fun registerForRemoteNotifications() {
-        logInfo("Hello, World")
+    public fun registerForNotifications(initialChannels: Set<String> = emptySet()) {
+        if (isInitialized) {
+            registerForNotificationsOnChannels(registeredChannels)
+        } else {
+            registerForNotificationsOnChannels(initialChannels)
+        }
+    }
+
+    public fun addRegisteredChannel(channel: String) {
+        val channels = registeredChannels.toMutableSet()
+        channels.add(channel)
+
+        registerForNotificationsOnChannels(channels)
+    }
+
+    public fun removeRegisteredChannel(channel: String) {
+        val channels = registeredChannels.toMutableSet()
+        channels.remove(channel)
+
+        registerForNotificationsOnChannels(channels)
     }
 
     public fun registerForNotificationsOnChannels(channels: Set<String>) {
-        logInfo("Registering for notifications on channels $channels")
+        logInfo("Registering for notifications on channels ${channels.sorted()}")
+
+        NotificationListenerService.registerGcmReceiver(context)
 
         object : AsyncTask<Unit, Unit, Unit>() {
             override fun doInBackground(vararg backgroundParams: Unit) {
@@ -42,8 +63,16 @@ public class NotificationCenter(val context: Context, val senderIdentifier: Stri
 
     /// Managing the Channels
 
+    public val isInitialized: Boolean
+        get() = preferences.getBoolean("initialized", false)
+
+    public val registeredChannels: Set<String>
+        get() = preferences.getStringSet("channels", emptySet())
+
     private fun updateRegisteredChannels(channels: Set<String>) {
-
+        preferences.edit()
+                .putStringSet("channels", channels)
+                .putBoolean("initialized", true)
+                .commit()
     }
-
 }
--- notifications/src/main/java/ch/konoma/notifications/NotificationListenerService.kt
+++ notifications/src/main/java/ch/konoma/notifications/NotificationListenerService.kt
@@ -1,7 +1,12 @@
 package ch.konoma.notifications
 
+import android.content.Context
+import android.content.IntentFilter
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.google.android.gms.gcm.GcmListenerService
+import com.google.android.gms.gcm.GcmReceiver
 
 
 public class NotificationListenerService : GcmListenerService() {
@@ -11,5 +16,44 @@ public class NotificationListenerService : GcmListenerService() {
 
         //        val message = data?.getString("alert") ?: ""
         //        val id = data?.getString("messageId")?.toInt() ?: 0
+    }
+
+    companion object {
+
+        private var hasRegisteredReceiver = false
+
+        public fun registerGcmReceiver(context: Context) {
+            if (hasRegisteredReceiver) {
+                return
+            }
+
+            // This is equivalent to the following declaration in the manifest:
+            //
+            // <receiver
+            //   android:name="com.google.android.gms.gcm.GcmReceiver"
+            //   android:exported="true"
+            //   android:permission="com.google.android.c2dm.permission.SEND">
+            //
+            //     <intent-filter>
+            //         <action android:name="com.google.android.c2dm.intent.RECEIVE"/>
+            //         <action android:name="com.google.android.c2dm.intent.REGISTRATION"/>
+            //         <category android:name="${applicationId}"/>
+            //     </intent-filter>
+            // </receiver>
+
+            val intentFilter = IntentFilter()
+            intentFilter.addAction("com.google.android.c2dm.intent.RECEIVE")
+            intentFilter.addAction("com.google.android.c2dm.intent.REGISTRATION")
+            intentFilter.addCategory(context.packageName)
+
+            val permission = "com.google.android.c2dm.permission.SEND"
+
+            context.registerReceiver(
+                    GcmReceiver(),
+                    intentFilter,
+                    permission,
+                    Handler(Looper.getMainLooper())
+            )
+        }
     }
 }
